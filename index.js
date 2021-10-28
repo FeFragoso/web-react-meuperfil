8ec17068562f236c4aaf8656a3d80aba407409ea
Felipe Fragoso <62634029+FeFragoso@users.noreply.github.com>
2021-10-28 02:10:05 +0000

    1.5

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const porta = process.env.PORT || 3000
 
-
+app.get('/', (req, res)=> {
+  res.sendFile(path.join(__dirname, '/front/meuPerfil.html'))
+})
 
 app.listen(porta, ()=> {console.log("Servidor Rodando")})
